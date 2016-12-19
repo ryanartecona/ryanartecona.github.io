ff40e4221e8f7bc75ad112e1a499e5f141c87354
Ryan Artecona <ryanartecona@gmail.com>
2016-12-19 09:55:08 -0600

    Reconfigure post URLs, redirect old URLs
    - Old post URLs were from Jekyll defaults, like:
        /2013/10/26/post-title-here/index.html
      Then in introducing Hakyll they became:
        _posts/2013-10-26-post-title-here.html
      New ones are like:
        /post/2013/10/26/post-title-here/index.html
    
    - We strip trailing "/index.html" off any links which have it to get
      extension-free URLs like Jekyll had:
        /post/2013/10/26/post-title-here
    
    - URLs for existing posts get a page which redirects a browser to the
      new-style URL, which is the best I can do without a 301 redirect.

--- _src/site.hs
+++ _src/site.hs
@@ -1,5 +1,6 @@
---------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
+--------------------------------------------------------------------------------
+module Main where
 import           Text.Pandoc.Options
 import           Text.Pandoc.Definition
 import           Flow
@@ -7,7 +8,7 @@ import           Data.Char (toLower)
 import           Data.Monoid ((<>))
 import           Hakyll
 import           Hakyll.Web.Sass (sassCompiler)
-import qualified System.FilePath as FP
+import           System.FilePath
 import           System.Process (readProcess)
 
 
@@ -30,7 +31,11 @@ main = hakyll $ do
       compile templateCompiler
 
     match "_posts/*" $ do
-      route $ setExtension "html"
+      route $ foldl composeRoutes idRoute [
+        setExtension "html",
+        customRoute $ \p -> p |> toFilePath |> (`replaceDirectory` "post/"),
+        appendIndex,
+        dateFolders]
       compile $ getResourceBody
         >>= saveSnapshot "source"
         >>  myPandocCompiler
@@ -39,7 +44,7 @@ main = hakyll $ do
         >>= relativizeUrls
 
     match "about.html" $ do
-      route $ customRoute $ \t -> t |> toFilePath |> FP.dropExtension |> (FP.</> "index.html")
+      route $ customRoute $ \t -> t |> toFilePath |> dropExtension |> (</> "index.html")
       compile $ myPandocCompiler
         >>= loadAndApplyTemplate "_layouts/default.html" defaultContext
         >>= relativizeUrls
@@ -56,28 +61,23 @@ main = hakyll $ do
           >>= loadAndApplyTemplate "_layouts/default.html" indexCtx
           >>= relativizeUrls
 
-    -- create ["archive.html"] $ do
-    --     route idRoute
-    --     compile $ do
-    --         posts <- recentFirst =<< loadAll "posts/*"
-    --         let archiveCtx =
-    --                 listField "posts" postCtx (return posts) `mappend`
-    --                 constField "title" "Archives"            `mappend`
-    --                 defaultContext
-    --         makeItem ""
-    --             >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-    --             >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-    --             >>= relativizeUrls
+    redirect "2013/10/26/decorated-anonymous-functions-in-python/index.html"
+             "_posts/2013-10-26-decorated-anonymous-functions-in-python.md"
+
+    redirect "2015/05/21/refactoring-in-ruby-in-haskell/index.html"
+             "_posts/2015-05-21-refactoring-in-ruby-in-haskell.md"
 
 
 --------------------------------------------------------------------------------
 -- Helpers and Context fields
+
 dateCtx :: Context String
 dateCtx = dateField "date" "%B %e, %Y"
 
 postCtx :: Context String
 postCtx =
     dateCtx
+    <> dropIndexHtml "url"
     <> defaultContext
 
 myPandocCompiler :: Compiler (Item String)
@@ -87,6 +87,17 @@ myPandocCompiler =
     defReadOpts = defaultHakyllReaderOptions
     defWriteOpts = defaultHakyllWriterOptions {writerEmailObfuscation = NoObfuscation}
 
+redirect :: Identifier -> Identifier -> Rules ()
+redirect fromPath toItem =
+  create [fromPath] $ do
+    route idRoute
+    compile $ do
+      item <- (load toItem) :: Compiler (Item String)
+      maybeRoute <- item |> itemIdentifier |> getRoute
+      r <- maybe (fail "unknown route for redirect target") return maybeRoute
+      let ctx = constField "location" r
+      makeItem ""
+        >>= loadAndApplyTemplate "_layouts/redirect.html" ctx
 
 --------------------------------------------------------------------------------
 -- Highlight with ambient "pygmentize" executable
@@ -105,3 +116,20 @@ pygments code options =
     (lang:_) ->
       readProcess "pygmentize" ["-l", toLower <$> lang,  "-f", "html"] code
     _ -> return $ "<div class =\"highlight\"><pre>" ++ code ++ "</pre></div>"
+
+
+--------------------------------------------------------------------------------
+-- Jekyll-style post routing helpers
+
+appendIndex :: Routes
+appendIndex = customRoute $
+  (\(p, e) -> p </> "index" <.> e) . splitExtension . toFilePath
+
+dropIndexHtml :: String -> Context a
+dropIndexHtml key = mapContext transform (urlField key) where
+    transform url = case splitFileName url of
+                        (p, "index.html") -> takeDirectory p
+                        _                 -> url
+dateFolders :: Routes
+dateFolders =
+    gsubRoute "/[0-9]{4}-[0-9]{2}-[0-9]{2}-" $ replaceAll "-" (const "/")
